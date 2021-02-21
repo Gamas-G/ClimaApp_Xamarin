e5cc2f4e123b90ae017ec0ae559314e30c76d341
Gerardo Gamas <kukulcan_gamas@hotmail.com>
2021-02-20 21:04:34 -0500

    diseno ClimaApp

---
+++ ClimaApp/ClimaApp/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ClimaApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+
+            InitializeComponent();
+
+            this.BindingContext = this;
+
+        }
+
+        /*Esto es interesante si lo colocamos en el constructor en un refresh no carga los datos
+         *Con con este metodo y esta lista publica en el xaml lo esta adquiriendo por el binding
+         */
+        public List<Clima> LisWeather { get => DatosWeather(); }
+        private List<Clima> DatosWeather()
+        {
+            var miLista = new List<Clima>();
+            miLista.Add(new Clima() { Dia = "Domingo 21", Temperatura = "27", Icono = "weather" });
+            miLista.Add(new Clima() { Dia = "Lunes 22", Temperatura = "29", Icono = "weather" });
+            miLista.Add(new Clima() { Dia = "Martes 23", Temperatura = "28", Icono = "weather" });
+            miLista.Add(new Clima() { Dia = "Miercoles 24", Temperatura = "28", Icono = "weather" });
+            miLista.Add(new Clima() { Dia = "Jueves 25", Temperatura = "28", Icono = "weather" });
+            miLista.Add(new Clima() { Dia = "Viernes 26", Temperatura = "28", Icono = "weather" });
+            miLista.Add(new Clima() { Dia = "Sábado 27", Temperatura = "29", Icono = "weather" });
+            return miLista;
+        }
+
+    }
+
+    public class Clima
+    {
+        public string Dia { set; get; }
+        public string Temperatura { set; get; }
+        public string Icono { set; get; }
+    }
+}
